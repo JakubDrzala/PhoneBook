3a7a39401dd8b6d545fa7fe3471a6bbaffe703aa
RybakProgramista <118109173+RybakProgramista@users.noreply.github.com>
2023-10-18 09:17:07 +0200

    szczur upgrade

---
+++ Szczur/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+public class Main {
+    public static void main(String[] args) {
+        JFrame mainFrame = new JFrame("Ratte");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setResizable(false);
+        mainFrame.setSize(512, 512);
+        mainFrame.setVisible(true);
+    }
+}
