5400ad5d90f0b68c3ba116e4559d5b8c98ac18e8
RybakProgramista <118109173+RybakProgramista@users.noreply.github.com>
2023-10-18 12:16:19 +0200

    organaising

---
+++ Source/PhoneBook/src/main/java/org/example/Controller/Search.java
@@ -0,0 +1,14 @@
+package org.example.Controller;
+
+import java.sql.*;
+
+public class Search {
+    private Connection con;
+    public Search(Connection con){
+        this.con = con;
+    }
+
+    public void find(String querry){
+
+    }
+}
