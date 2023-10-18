c7fb7ea9ee1095c2425278f9f8ec8dccf939bd75
RybakProgramista <118109173+RybakProgramista@users.noreply.github.com>
2023-10-18 12:59:02 +0200

    search extending 1

--- Source/PhoneBook/src/main/java/org/example/Controller/Search.java
+++ Source/PhoneBook/src/main/java/org/example/Controller/Search.java
@@ -1,10 +1,21 @@
 package org.example.Controller;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Search extends DatabaseExtention{
     public Search(Connection con) {
         super(con);
+    }
+
+    public void querryBuilder(List<String> inputs){
+        for(String input : inputs){
+            if(!input.contains("@")){
+                input = input.toLowerCase();
+                input = input.replace(input.toCharArray()[0], input.toUpperCase().toCharArray()[0]);
+            }
+        }
     }
 
     public void find(String querry) throws SQLException{
--- Source/PhoneBook/src/main/java/org/example/Main.java
+++ Source/PhoneBook/src/main/java/org/example/Main.java
@@ -1,9 +1,14 @@
 package org.example;
 
+import java.sql.SQLException;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        run();
+    }
+    private static void run() throws SQLException {
 
     }
 }
