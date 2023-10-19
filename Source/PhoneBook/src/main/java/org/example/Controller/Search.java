8637febe9a71e220a222f2a4369cb220e0512f2a
RybakProgramista <118109173+RybakProgramista@users.noreply.github.com>
2023-10-19 10:54:38 +0200

    jebać

--- Source/PhoneBook/src/main/java/org/example/Controller/Search.java
+++ Source/PhoneBook/src/main/java/org/example/Controller/Search.java
@@ -3,25 +3,22 @@ package org.example.Controller;
 import java.sql.*;
 import java.util.List;
 
-public class Search extends DatabaseExtention {
+public class Search extends DatabaseExtentsion {
     public Search(Connection con) { //constructor for search based on abstract class
         super(con);
     }
 
-    public void querryBuilder(List<String> inputs) throws SQLException{ //building querry for selecting in db
+    public String querryBuilder(List<String> inputs) throws SQLException{ //building querry for selecting in db
         //getting name of table to prevent dumbass customer from misspelling word EMPLOYEES in db bcs yes, and using language that we are not familiar with, bcs WHY THE FUCK NOT USE LANGUAGE THAT NO ONE GIVES A FUCK ABOUT IN FUCKING DATABASE IN WHICH EVERYTHING SHOULD BE NAMED, AND BY EVERYTHING I MEAN EVERYFUCKINGTHING, IN THE MOST INTERNATIONAL LANGUAGE WHICH IS ENGLISH
         String[] types = {"TABLE"};
         ResultSet tableNameRs = getCon().getMetaData().getTables(null, null, "%", types);
         String tableName = tableNameRs.getString(0);
 
+        //i am to lazy to use tokenization so i will make sure that each input is the same as data in db
         for(String input : inputs){
             input = input.toUpperCase();
         }
-    }
+        return null;
 
-    public void find(String querry) throws SQLException{ //passing builded querry to printResult function
-        Statement stmt = getCon().createStatement();
-        ResultSet rt = stmt.executeQuery(querry);
-        printResult(rt);
     }
 }
--- Source/PhoneBook/src/main/java/org/example/Main.java
+++ Source/PhoneBook/src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import org.example.Controller.Search;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -16,7 +17,9 @@ public class Main {
         Connection con = DriverManager.getConnection(url, username, password);
         //up here is basic db connection that will be used often so dont fuck this up
 
+        System.out.println("SPERMA");
         Search search = new Search(con);
         //and here we have objects that are basically core for our project, each programmed by different co-workers so i really hope that this will cooperate
+        List<String> gowno = search.getColumn("SELECT * from employees", 3);
     }
 }
