38bd75e8e9f385601d3a850982a06641e382d55b
RybakProgramista <118109173+RybakProgramista@users.noreply.github.com>
2023-10-19 13:10:02 +0200

    s

--- Source/PhoneBook/src/main/java/org/example/Main.java
+++ Source/PhoneBook/src/main/java/org/example/Main.java
@@ -17,7 +17,6 @@ public class Main {
         Connection con = DriverManager.getConnection(url, username, password);
         //up here is basic db connection that will be used often so dont fuck this up
 
-        System.out.println("SPERMA");
         Search search = new Search(con);
         //and here we have objects that are basically core for our project, each programmed by different co-workers so i really hope that this will cooperate
         List<String> gowno = search.getColumn("SELECT * from employees", 3);
