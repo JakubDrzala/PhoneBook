f16d7d86af55708e1ea5c91c3138444a084f74a0
RybakProgramista <118109173+RybakProgramista@users.noreply.github.com>
2023-10-19 13:25:49 +0200

    napraw to

--- Source/PhoneBook/src/main/java/org/example/Main.java
+++ Source/PhoneBook/src/main/java/org/example/Main.java
@@ -11,13 +11,14 @@ import java.util.List;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws SQLException {
-        String url = "jdbc:mysql://localhost:3306/ubercompany";
-        String username = "root";
+
+        String dbURL = "jdbc:derby://localhost/employees;create=true";
+        String user = "root";
         String password = "";
-        Connection con = DriverManager.getConnection(url, username, password);
+        Connection conn = DriverManager.getConnection(dbURL, user, password);
         //up here is basic db connection that will be used often so dont fuck this up
 
-        Search search = new Search(con);
+        Search search = new Search(conn);
         //and here we have objects that are basically core for our project, each programmed by different co-workers so i really hope that this will cooperate
         List<String> gowno = search.getColumn("SELECT * from employees", 3);
     }
