582a8a06469f0cb3bfe5ff4f63c2d36c06cb471b
JakubDrzala <96517502+JakubDrzala@users.noreply.github.com>
2023-10-23 10:55:11 +0200

    cos zrobione z dodawaniem ale nie działa
    nie wiem kiedy będzie działać <3

--- Layout/PhoneBook/src/main/java/org/example/Controller/Add.java
+++ Layout/PhoneBook/src/main/java/org/example/Controller/Add.java
@@ -5,5 +5,15 @@ public class Add extends DatabaseExtentsion{
     public Add(Connection con) { //constructor for search based on abstract class
         super(con);
     }
+    public static void insert(String name, String surname, String number, String email){
 
+        //Connection conn=ConnectDB();
+        try{
+            String query = "insert into customer values (null, '"+name+"', "+surname+"', '"+number+"', '"+email+"', '"+email+"')";
+
+
+        }catch(Exception e){
+            System.out.println("coś jebło");
+        }
+    }
 }
