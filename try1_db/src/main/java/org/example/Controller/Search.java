9f9694fb62631804fefddfde79f83339658466ec
RybakProgramista <118109173+RybakProgramista@users.noreply.github.com>
2023-10-26 09:30:14 +0200

    :)

--- try1_db/src/main/java/org/example/Controller/Search.java
+++ try1_db/src/main/java/org/example/Controller/Search.java
@@ -9,7 +9,6 @@ public class Search extends DatabaseExtentsion {
     }
 
     private String querryBuilder(List<String> inputs) throws SQLException{ //building querry for selecting in db
-        //getting name of table to prevent dumbass customer from misspelling word EMPLOYEES in db bcs yes, and using language that we are not familiar with, bcs WHY THE FUCK NOT USE LANGUAGE THAT NO ONE GIVES A FUCK ABOUT IN FUCKING DATABASE IN WHICH EVERYTHING SHOULD BE NAMED, AND BY EVERYTHING I MEAN EVERYFUCKINGTHING, IN THE MOST INTERNATIONAL LANGUAGE WHICH IS ENGLISH
 
         boolean isNull = true;
         //i am to lazy to use tokenization so i will make sure that each input is the same as data in db
